a0116078a9295b59a9c8382c3c3c3feb5e4ce888
pedroangelini <pedro.angelini@gmail.com>
2022-09-16 11:53:13 -0500

    enhancing documentation of functions

--- pedro_scripts.py
+++ pedro_scripts.py
@@ -162,6 +162,24 @@ def pbi_teams_url(url="", print_or_return="print", copy_to_clipboard=True):
 
 
 def generate_sql_snapshot_insert_values(dfrow):
+    """Generates a line of the VALUES part of an INSERT query in the
+    Resource Tracker snapshot table, replacing nas for NULLs
+
+    Args:
+        dfrow (pandas.Series): dataframe row containing all columns from the resource Tracker
+        namely: 'sapid', 'period', 'month', 'employee_name', 'employment_status',
+        'employee_group', 'employee_subgroup_code', 'employee_subgroup_text',
+        'contract_end', 'service_date', 'company_code', 'company_code_name',
+        'cost_center_code', 'cost_center_name', 'cost_center_category',
+        'position_id', 'position_name', 'global_role_non_na_cost', 'superior_sapid',
+        'superior', 'organization', 'stream', 'function', 'opco', 'reason_for_leaving',
+        'business_resource', 'global_resource_work', 'personnel_administrator',
+        'location', 'location_description', 'building', 'building_name', 'status',
+        'hybrid_scope', 'comment', 'userid', 'email_address',
+
+    Returns:
+        str: 1 item of the VALUES part of the INSERT query
+    """
     ret = f"({dfrow['sapid']}, '{dfrow['period']}', '{dfrow['month']}', '{dfrow['employee_name']}', '{dfrow['employment_status']}', "
     ret += f"'{dfrow['employee_group']}', '{dfrow['employee_subgroup_code']}', '{dfrow['employee_subgroup_text']}', "
     ret += f"'{dfrow['contract_end']}', '{dfrow['service_date']}', '{dfrow['company_code']}', '{dfrow['company_code_name']}', "
@@ -176,6 +194,23 @@ def generate_sql_snapshot_insert_values(dfrow):
 
 
 def generate_sql_insert_snapshot(df):
+    """Creates an INSERT query for the resource_tracker_snapshot table
+    based on a dataframe with the right columns
+
+    Args:
+        df (pandas.DataFrame): df containing the lines to be inserted, with the
+        following columns: 'sapid', 'period', 'month', 'employee_name', 'employment_status',
+        'employee_group', 'employee_subgroup_code', 'employee_subgroup_text',
+        'contract_end', 'service_date', 'company_code', 'company_code_name',
+        'cost_center_code', 'cost_center_name', 'cost_center_category',
+        'position_id', 'position_name', 'global_role_non_na_cost', 'superior_sapid',
+        'superior', 'organization', 'stream', 'function', 'opco', 'reason_for_leaving',
+        'business_resource', 'global_resource_work', 'personnel_administrator',
+        'location', 'location_description', 'building', 'building_name', 'status',
+        'hybrid_scope', 'comment', 'userid', 'email_address',
+
+    Returns: the INSERT statment as a string
+    """
     ret = """INSERT INTO [vcort].[resource_tracker_snapshot]
            ([sapid]
            ,[period]
@@ -259,7 +294,7 @@ def make_json(csvFilePath, jsonFilePath, key_column):
 
 
 def flatten(list_of_lists):
-    """_summary_
+    """Flattens a list of lists (2 levels)
 
     Args:
         list_of_lists (list): list of lists (ie: [ [a,b], [c], []])
